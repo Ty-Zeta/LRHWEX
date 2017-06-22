8b32b4095215bec9e649cdef77409cf5139a8aaa
Ty-Zeta <tyofzeta@gmail.com>
2017-06-22 13:17:08 -0400

    LREX1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing"
+puts "I'd much rather you 'not'"
+puts 'I "said" do not touch.'
+puts "Hello my baby, hello my darling, hello my rag time gal"
+# This makes notes & doesn't affect the code
