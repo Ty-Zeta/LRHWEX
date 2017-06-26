cd75af169607184b0c4c54da880e02af88befe2c
Ty-Zeta <tyofzeta@gmail.com>
2017-06-26 14:00:32 -0400

    lrhwex7

---
+++ ex7.rb
@@ -0,0 +1,25 @@
+# write mary had a little lamb
+puts "Mary had a little lamb."
+# write its fleece was white as snow. Single quote marks tells ruby to ignore variables
+puts "Its fleece was white as #{'snow'}"
+# write and everywhere that Mary went
+puts "And everywhere that Mary went."
+#this command writes out . ten times individually
+puts "." * 10 # what'd that do? It's kinda like 10.times do, places . ten times
+# end 1-12 are each equated with the individual variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+# each of the math addition equations will spell out the words
+#watch that print vs. puts on this line what's it do? it doesn't skip a line and places it next to the first line
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end11 + end12
