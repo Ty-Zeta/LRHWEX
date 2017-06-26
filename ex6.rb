b9094a42febb3292e586982eb7199b11634a1d97
Ty-Zeta <tyofzeta@gmail.com>
2017-06-26 13:25:35 -0400

    LREX6

---
+++ ex6.rb
@@ -0,0 +1,28 @@
+#set the variable of types of people to 10
+types_of_people = 10
+#variable of x is the statement of how many people there are with the variable types of people
+x = "There are #{types_of_people} types of people."
+#set binary as binary
+binary = "binary"
+#set do not as don't
+do_not = "don't"
+# y is set up as the punchline string containing the other variables
+y = "Those who know #{binary} and those who #{do_not}."
+#outputing x and y
+puts x
+puts y
+#write I said x
+puts "I said: #{x}."
+#write I also said 7
+puts "I also said: '#{y}'."
+#set hilarious to false
+hilarious = false
+#set joke_evaluation to isnt that joke so funny?
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#write the function joke_evaluation with the variable hilarious
+puts joke_evaluation
+#set w & e as these two statements
+w = "This is the left side of..."
+e = "a string with a right side."
+# w & e are used as string explanations used with a math formula to explain sides
+puts w + e
