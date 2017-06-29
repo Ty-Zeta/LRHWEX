a4ae119072fe4bcae5a97c5d4b0196b350cfe5c0
Ty-Zeta <tyofzeta@gmail.com>
2017-06-29 11:30:30 -0400

    LREX11

---
+++ ex11.rb
@@ -0,0 +1,10 @@
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp
+print "What color are your eyes?"
+eyes = gets.chomp
+# I added an oxford comman because I'm not a barbarian
+puts "So, you're #{age} old, #{height} tall, have #{eyes} eyes, and #{weight} heavy."
